389dbf76fd2c54d6f8fc794d6b42a8fa7e58fcae
priyarkp <rkp9449@gmail.com>
2026-03-10 14:49:11 -0400

    chore: reorg prettier and task

--- src/models/task.ts
+++ src/models/task.ts
@@ -4,13 +4,13 @@ export type TaskStatus = 'pending' | 'completed';
 export type FamilyMembers = 'mom' | 'dad' | 'tiny1' | 'tiny2';
 export type Priority = 'urgent' | 'whenever' | 'meh';
 export interface Task {
-    readonly id : string;
-    title : string;
-    source: TaskSource;
-    type: TaskType;
-    status: TaskStatus;
-    assignedTo: FamilyMembers;
-    priority: Priority;
-    dueDate ?: Date;
-    description?: string;
+  readonly id: string;
+  title: string;
+  source: TaskSource;
+  type: TaskType;
+  status: TaskStatus;
+  priority: Priority;
+  assignedTo?: FamilyMembers;
+  dueDate?: Date;
+  description?: string;
 }
